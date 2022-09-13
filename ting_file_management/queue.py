b78d9f3e3f82ffef9d475d006db8482eb900236f
Pedro Lima <pedro.oficial1969@gmail.com>
2022-09-13 16:49:38 -0300

    feat(requisito-1): adiciona requisito completo

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,19 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+      self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        current_data = self._data[0]
+        del self._data[0]
+        return current_data
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index in range(self.__len__()):
+          return self._data[index]
+        raise IndexError
