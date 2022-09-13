651c83d26842a365aa7e13d592a61c5e05c3906e
Pedro Lima <pedro.oficial1969@gmail.com>
2022-09-13 20:37:13 -0300

    feat(requisitos 3,4 e 5): adiciona requisitos completos

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -6,18 +6,16 @@ def txt_importer(path_file):
         list_character_path = path_file.split('.')
         last_index = len(list_character_path) - 1
         current_values = []
-  
+
         if list_character_path[last_index] != 'txt':
             return sys.stderr.write("Formato inválido\n")
-  
+
         with open(path_file) as file:
             lines = file.readlines()
-            print(lines)
-  
+
             for line in lines:
-            
                 current_values.append(line.strip("\n"))
-  
+
         return current_values
     except FileNotFoundError:
         return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,34 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    list_file = txt_importer(path_file)
+    if path_file not in instance._data:
+      instance.enqueue(path_file)
+
+      return sys.stdout.write(str({
+          "nome_do_arquivo": path_file,
+          "qtd_linhas": len(list_file),
+          "linhas_do_arquivo": list_file,   
+      }))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
-
+    if len(instance) < 1:
+        return sys.stdout.write("Não há elementos\n")
+    
+    current_file = instance.dequeue()
+    return sys.stdout.write(str(f"Arquivo {current_file} removido com sucesso\n"))
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+      current_instance = instance.search(position)
+      list_file = txt_importer(current_instance)
+          
+      return sys.stdout.write(str({
+            "nome_do_arquivo": current_instance,
+            "qtd_linhas": len(list_file),
+            "linhas_do_arquivo": list_file,
+      }))
+    except IndexError:
+      return sys.stderr.write(str("Posição inválida\n"))
