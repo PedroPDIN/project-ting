3ab6048862358fbbf26d1f716bb3ed05c8ebfb81
Pedro Lima <pedro.oficial1969@gmail.com>
2022-09-14 19:04:10 -0300

    feat: adiciona alterações ao arquivo file_process e requisito 6 completo.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,21 +4,28 @@ from ting_file_management.file_management import txt_importer
 
 def process(path_file, instance):
     list_file = txt_importer(path_file)
-    if path_file not in instance._data:
-        instance.enqueue(path_file)
+    file_found = True
+    current_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(list_file),
+        "linhas_do_arquivo": list_file,
+    }
 
-        return sys.stdout.write(str({
-            "nome_do_arquivo": path_file,
-            "qtd_linhas": len(list_file),
-            "linhas_do_arquivo": list_file,
-        }))
+    for element in range(len(instance)):
+        if instance.search(element)["nome_do_arquivo"] == path_file:
+            file_found = False
+
+    if file_found:
+        instance.enqueue(current_dict)
+
+    return sys.stdout.write(str(current_dict))
 
 
 def remove(instance):
     if len(instance) < 1:
         return sys.stdout.write("Não há elementos\n")
 
-    current_file = instance.dequeue()
+    current_file = instance.dequeue()["nome_do_arquivo"]
     return sys.stdout.write(str(
       f"Arquivo {current_file} removido com sucesso\n"
     ))
@@ -27,12 +34,7 @@ def remove(instance):
 def file_metadata(instance, position):
     try:
         current_instance = instance.search(position)
-        list_file = txt_importer(current_instance)
 
-        return sys.stdout.write(str({
-            "nome_do_arquivo": current_instance,
-            "qtd_linhas": len(list_file),
-            "linhas_do_arquivo": list_file,
-        }))
+        return sys.stdout.write(str(current_instance))
     except IndexError:
         return sys.stderr.write(str("Posição inválida\n"))
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    lines_found = []
+
+    for value in range(len(instance)):
+        current_line = instance.search(value)
+        for item, line in enumerate(current_line["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                lines_found.append({"linha": item + 1})
+
+        if lines_found:
+            result.append({
+                "palavra": f"{word}",
+                "arquivo": current_line["nome_do_arquivo"],
+                "ocorrencias": lines_found,
+            })
+
+    return result
 
 
 def search_by_word(word, instance):
